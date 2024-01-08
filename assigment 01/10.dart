cd85b9454680878bfee1637c489e6c57371ceee1
malahimansari <malahimtech@gmail.com>
2024-01-08 13:42:47 +0500

    Get Input for the user

--- assigment 01/10.dart
+++ assigment 01/10.dart
@@ -4,6 +4,22 @@
 import 'dart:io';
 
 void main() {
-  stdout.write('Enter first number: ');
-  // int  = int.parse(stdin.readLineSync()!);
+  stdout.write('Enter the first number: ');
+  double num1 = double.parse(stdin.readLineSync()!);
+
+  stdout.write('Enter the second number: ');
+  double num2 = double.parse(stdin.readLineSync()!);
+
+  stdout.write('Enter the third number: ');
+  double num3 = double.parse(stdin.readLineSync()!);
+
+  // Finding the greatest number
+  double greatest =
+      (num1 > num2) ? (num1 > num3 ? num1 : num3) : (num2 > num3 ? num2 : num3);
+  print('The greatest number is: $greatest');
+
+  // Finding the lowest number
+  double lowest =
+      (num1 < num2) ? (num1 < num3 ? num1 : num3) : (num2 < num3 ? num2 : num3);
+  print('The lowest number is: $lowest');
 }
