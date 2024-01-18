427e43ad1f5d68a7ea8cdc494874fbad5679d010
malahimansari <malahimtech@gmail.com>
2024-01-18 21:01:41 +0500

    print data into list of map using for loop

--- Practice/loops.dart
+++ Practice/loops.dart
@@ -2,4 +2,14 @@ void main() {
   for (int i = 0; i < 3; i++) {
     print(i);
   }
+
+  List<Map> data = [
+    {"name": "Malahim", "age": 20, "City": "Karachi"},
+    {"name": "Abbas", "age": 22, "City": "Hyderabad"},
+    {"name": "Shaheer", "age": 18, "City": "Lahore"},
+  ];
+
+  for (int i = 0; i < data.length; i++) {
+    (i == data.length - 1) ? print(data[i]) : print(data[i]["name"]);
+  }
 }
