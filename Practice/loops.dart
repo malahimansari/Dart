c0b3d73b9728a9e978eae7dd6302817714b55531
malahimansari <malahimtech@gmail.com>
2024-01-23 23:49:09 +0500

    While Loop

--- Practice/loops.dart
+++ Practice/loops.dart
@@ -1,23 +1,39 @@
 void main() {
-  for (int i = 0; i < 3; i++) {
-    print(i);
+  // for (int i = 0; i < 3; i++) {
+  //   print(i);
+  // }
+
+  // List<Map> data = [
+  //   {"name": "Malahim", "age": 20, "City": "Karachi"},
+  //   {"name": "Abbas", "age": 22, "City": "Hyderabad"},
+  //   {"name": "Shaheer", "age": 18, "City": "Lahore"},
+  // ];
+
+  // for (int i = 0; i < data.length; i++) {
+  //   (i == data.length - 1) ? print(data[i]) : print(data[i]["name"]);
+  // }
+
+  // List<Map> obj = [
+  //   {"name": "Malahim", "age": 20, "City": "Karachi"},
+  //   {"name": "Abbas", "age": 22, "City": "Hyderabad"},
+  //   {"name": "Shaheer", "age": 18, "City": "Lahore"},
+  // ];
+
+  // print(obj);
+  String email = "abcd@gmail.com";
+  String password = "123";
+
+  bool whileLoop = true;
+  while (whileLoop) {
+    if (email == "abc@gmail.com" && password == "123") {
+      print("login successfull");
+      whileLoop = false;
+    } else {
+      print("login failed");
+      print("try again");
+      print("=====================");
+      email = "abc@gmail.com";
+      password = "123";
+    }
   }
-
-  List<Map> data = [
-    {"name": "Malahim", "age": 20, "City": "Karachi"},
-    {"name": "Abbas", "age": 22, "City": "Hyderabad"},
-    {"name": "Shaheer", "age": 18, "City": "Lahore"},
-  ];
-
-  for (int i = 0; i < data.length; i++) {
-    (i == data.length - 1) ? print(data[i]) : print(data[i]["name"]);
-  }
-
-  List<Map> obj = [
-    {"name": "Malahim", "age": 20, "City": "Karachi"},
-    {"name": "Abbas", "age": 22, "City": "Hyderabad"},
-    {"name": "Shaheer", "age": 18, "City": "Lahore"},
-  ];
-
-  print(obj);
 }
