40ebcb952f77f814c6cd5a044098778f13baad5b
malahimansari <malahimtech@gmail.com>
2024-01-25 13:54:25 +0500

    Create a list of names and print all names

---
+++ assigment 02/q1.dart
@@ -0,0 +1,9 @@
+// Q.1: Create a list of names and print all names using list.
+
+void main() {
+  List<String> names = ["Shayan", "Malahim", "Basit", "Shahzaib"];
+
+  for (String name in names) {
+    print(name);
+  }
+}
