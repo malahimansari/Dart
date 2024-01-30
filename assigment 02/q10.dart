c9b7386ba7557a8ee3c1cff700ca9b58184110e5
malahimansari <malahimtech@gmail.com>
2024-01-30 14:40:02 +0500

    Removes Duplicates on List and Create a new List

---
+++ assigment 02/q10.dart
@@ -0,0 +1,18 @@
+// Q.10: Write a Dart code that takes in a list of strings and removes any duplicate
+// elements, returning a new list without duplicates. The order of elements in the
+// new list should be the same as in the original list.
+void main() {
+  List<String> originalList = [
+    "apple",
+    "banana",
+    "orange",
+    "apple",
+    "grape",
+    "banana",
+    "kiwi",
+    "apple"
+  ];
+
+  List<String> uniqueList = originalList.toSet().toList();
+  print(uniqueList);
+}
