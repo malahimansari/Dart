43e9a5eb2eef0c87c4fd3e4281fba0d3e7baad9c
malahimansari <malahimtech@gmail.com>
2024-02-04 21:36:44 +0500

    Check Product is Stock or In Stock

---
+++ assigment 02/q19.dart
@@ -0,0 +1,16 @@
+// Q.19: Given a map representing a product with keys "name", "price", and
+// "quantity", write Dart code to check if the product is in stock. If the quantity is
+// greater than 0, print "In stock", otherwise print "Out of stock".
+
+void main() {
+  Map<String, dynamic> product = {
+    "name": "Iphone",
+    "price": 1000,
+    "quantity": 10
+  };
+  if (product["quantity"] <= 0) {
+    print("Out of Stock");
+  } else {
+    print("In Stock");
+  }
+}
