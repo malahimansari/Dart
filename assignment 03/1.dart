9847c83a3727f21086a1e211deb5e76d978c8838
malahimansari <malahimtech@gmail.com>
2024-02-05 19:59:39 +0500

    assignment 3

---
+++ assignment 03/1.dart
@@ -0,0 +1,7 @@
+// Write a program that takes a list of numbers as input and prints the even numbers in the list using a for loop.
+
+import 'dart:io';
+
+void main() {
+  // List<int> numbers = [];
+}
