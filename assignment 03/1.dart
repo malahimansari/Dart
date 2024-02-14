f922fb5bf26de37068aace7624f648976e760960
malahimansari <malahimtech@gmail.com>
2024-02-14 14:12:25 +0500

    Get Input as a List and Print Even Number List

--- assignment 03/1.dart
+++ assignment 03/1.dart
@@ -1,7 +1,13 @@
-// Write a program that takes a list of numbers as input and prints the even numbers in the list using a for loop.
+// Write a program that takes a list of numbers as input and prints the even numbers
+// in the list using a for loop.
 
 import 'dart:io';
 
 void main() {
-  // List<int> numbers = [];
+  List<int> numbers = stdin.readLineSync()!.split(',').map(int.parse).toList();
+  for (int i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 == 0) {
+      print(numbers[i]);
+    }
+  }
 }
