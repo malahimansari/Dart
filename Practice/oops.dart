ab657493a070b11afc24646012b92359d5df64c3
malahimansari <malahimtech@gmail.com>
2024-02-15 23:35:25 +0500

    Constructor

--- Practice/oops.dart
+++ Practice/oops.dart
@@ -1,11 +1,18 @@
 void main() {
   FirstClass obj = new FirstClass();
   obj.func1();
+  parameterClass(name: "Shaheer");
 }
 
 class FirstClass {
   String msg = "Hello World";
   func1() {
     print(msg);
+  }
+}
+
+class parameterClass {
+  parameterClass({required String name}) {
+    print(name);
   }
 }
