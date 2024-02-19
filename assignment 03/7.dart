3432bfb5b4080b86f079cf290ee1617bacafa023
malahimansari <malahimtech@gmail.com>
2024-02-19 23:25:33 +0500

    print a table

--- assignment 03/7.dart
+++ assignment 03/7.dart
@@ -1,1 +1,21 @@
-void main() {}
+// Write a program that prints the multiplication table of a given number
+// using a for loop.
+// Example:
+// Input: 5
+// Output:
+// 5 x 1 = 5
+// 5 x 2 = 10
+// 5 x 3 = 15
+// ...
+// 5 x 10 = 50
+
+import 'dart:io';
+
+void main() {
+  print("Enter Number");
+  int input = int.parse(stdin.readLineSync()!);
+  for (var i = 1; i <= 10; i++) {
+    int table = i * input;
+    print("$input x $i = $table");
+  }
+}
